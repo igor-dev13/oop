6f16389f4aae1d05fbba0b6f5c6c5bc441b65a03
igor_xp <igor_xp@mail.ru>
2016-09-29 23:36:04 +0300

    Вынес сравнение в отдельную функцию CompareFiles

--- lab1/compare/compare/compare.cpp
+++ lab1/compare/compare/compare.cpp
@@ -1,43 +1,37 @@
 #include "stdafx.h"
 
+#include <string>
 #include <fstream>
 #include <iostream>
 
 using namespace std;
 
-int main(int argc, char * argv[])
+int CompareFiles(const string & fileName1, const string & fileName2, string & error)
 {
-	ifstream firstFile(argv[1]), secondFile(argv[2]);
 	char ch1, ch2;
 	int currentLine = 1;
-
-	if (argc != 3)
-	{
-		cout << "Invalid arguments count\n"
-			<< "Usage: compare.exe <input file> <input file>\n";
-		return 1;
-	}
+	ifstream firstFile(fileName1), secondFile(fileName2);
 
 	if (!firstFile.is_open())
-	{
-		cout << "Failed to open " << argv[1] << " for reading\n";
-		return 1;
+	{	
+		error = error.append("Failed to open ").append(fileName1).append(" for reading");
+		return -1;
 	}
 
 	if (!secondFile.is_open())
 	{
-		cout << "Failed to open " << argv[2] << " for reading\n";
-		return 1;
-	}	
+		error = error.append("Failed to open ").append(fileName2).append(" for reading");
+		return -1;
+	}
 
 	auto ReadFiles = [&] {
 		firstFile.get(ch1);
 		secondFile.get(ch2);
 		return firstFile && secondFile;
 	};
-		
+
 	while (ReadFiles())
-	{	
+	{
 		if (ch1 == '\n' && ch2 == '\n')
 		{
 			++currentLine;
@@ -45,18 +39,52 @@ int main(int argc, char * argv[])
 
 		if (ch1 != ch2)
 		{
-			cout << "Files are different. Line number is " << currentLine;
-			return 1;
+			return currentLine;
 		}
 	}
 
 	if (firstFile || secondFile)
 	{
-		cout << "Files are different. Line number is " << currentLine;
+		return currentLine;
+	}	
+
+	return 0;
+}
+
+int main(int argc, char * argv[])
+{
+	string errorMessage = "";
+	const int FILE_COMPARISON_ERROR = -1;
+	const int FILES_ARE_EQUAL = 0;
+	const int FILES_ARE_DIFFERENT = 1;
+
+	if (argc != 3)
+	{
+		cout << "Invalid arguments count\n"
+			<< "Usage: compare.exe <input file> <input file>\n";
+		return 1;
+	}	
+
+	int result = CompareFiles(argv[1], argv[2], errorMessage);
+
+	if (result == FILE_COMPARISON_ERROR)
+	{
+		cout << errorMessage << "\n";
 		return 1;
 	}
+	else if (result >= FILES_ARE_DIFFERENT)
+	{
+		cout << "Files are different. Line number is " << result;
+		return 1;
+	}
+	else if (result == FILES_ARE_EQUAL)
+	{
+		cout << "Files are equal!\n";
+	}
+	else
+	{
+
+	}
 	
-		
-	cout << "Files are equal!\n";
 	return 0;
 }
