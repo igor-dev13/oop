7e10f215f7b9e00111d5eb464c35b0d70248d6b9
igor_xp <igor_xp@mail.ru>
2016-10-09 15:34:22 +0300

    Перенес проверку входящих параметров в функцию

--- lab1/rotatebyte/rotatebyte.cpp
+++ lab1/rotatebyte/rotatebyte.cpp
@@ -8,8 +8,46 @@
 
 using namespace std;
 
-int RotateNumber(int number, int rotationCount, char direction)
-{	
+const int BITE_ROTATE_ERROR = -1;
+
+int RotateNumber(const string & inputValue1, const string & inputValue2, char * inputValue3, string & error)
+{		
+	char direction = inputValue3[0];
+	
+	int number = atoi(inputValue1.c_str());
+	int rotationCount = atoi(inputValue2.c_str());
+
+	if (number == 0)
+	{
+		error = error.append("Enter a valid integer number for <bite>");
+		return -1;
+	}
+
+	if (rotationCount == 0)
+	{
+		error = error.append("Enter a valid integer number for  <number of bits>");
+		return -1;
+	}
+
+
+	if (direction != 'R' && direction != 'L')
+	{
+		error = error.append("Enter a valid direction, uppercase - L for left, or R for right");
+		return -1;
+	}
+
+	if (number > 255 || number < -255)
+	{
+		error = error.append("bite number range is from -255 to 255");
+		return -1;
+	}
+
+	if (rotationCount > 8 || rotationCount < 0)
+	{
+		error = error.append("bit count in bite is is from 0 to 8");
+		return -1;
+	}
+
 	for (int i = 0; i < rotationCount; i++)
 	{
 		number = (direction == 'R') ? number >> 1 : number << 1;
@@ -20,52 +58,26 @@ int RotateNumber(int number, int rotationCount, char direction)
 
 int main(int argc, char * argv[])
 {
+	string errorMessage = "";
+
 	if (argc != 4)
 	{
 		cout << "Invalid arguments count\n"
 			<< "Usage: rotatebyte.exe <byte> <number of bits> <L / R>\n";
 		return 1;
-	}
+	}	
 
-	string inputNumber = argv[1];
-	string inputCount = argv[2];
-	char * direction = argv[3];
-	int number = atoi(inputNumber.c_str());
-	int rotationCount = atoi(inputCount.c_str());
+	int result = RotateNumber(argv[1], argv[2], argv[3], errorMessage);
 
-	if (number == 0)
+	if (result == BITE_ROTATE_ERROR)
 	{
-		cout << "Enter a valid integer number for <bite>\n";
+		cout << errorMessage << "\n";
 		return 1;
 	}
-
-	if (rotationCount == 0)
+	else
 	{
-		cout << "Enter a valid integer number for  <number of bits>\n";
-		return 1;
+		cout << result;
 	}
-
-
-	if (*direction != 'R' && *direction != 'L')
-	{
-		cout << "Enter a valid direction, uppercase - L for left, or R for right\n";
-		return 1;
-	}
-
-	if (number > 255 || number < -255)
-	{
-		cout << "bite number range is from -255 to 255\n";
-		return 1;
-	}
-
-	if (rotationCount > 8 || rotationCount < 0)
-	{
-		cout << "bit count in bite is is from 0 to 8\n";
-		return 1;
-	}
-
-	int result = RotateNumber(number, rotationCount, *direction);
-	cout << result << '\n';	
 
 	return 0;
 }
