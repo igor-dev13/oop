0a4f9f59e2569602973b7a9419a12b03dd23214e
igor_xp <igor_xp@mail.ru>
2016-12-20 00:23:35 +0300

    Добавил каркас программы

---
+++ lab6/http_url/http_url_tests/stdafx.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define _USE_MATH_DEFINES
+
+#include "targetver.h"
+
+#include <boost/algorithm/string/replace.hpp>
+
+#define BOOST_TEST_INCLUDED
+#pragma warning (disable: 4702)
+#include <boost/test/unit_test.hpp>
